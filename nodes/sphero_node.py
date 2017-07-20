b8cc29df3777908f6f49191f831504415e0a5aa2
Darin Costello <dran226@gmail.com>
2017-07-19 19:42:26 -0600

    update to use new sdk/driver

--- nodes/sphero_node.py
+++ nodes/sphero_node.py
@@ -40,6 +40,8 @@ import tf
 import PyKDL
 
 from spheropy.Sphero import Sphero
+from spheropy.Exception import SpheroException
+from spheropy.DataStream import DataStreamManager
 import dynamic_reconfigure.server
 
 from sensor_msgs.msg import Imu
@@ -75,19 +77,31 @@ class SpheroNode(object):
         rospy.init_node('sphero_node')
         self.update_rate = default_update_rate
         self.sampling_divisor = int(400 / self.update_rate)
-
         self.is_connected = False
-        self._init_pubsub()
-        self._init_params()
 
-        self.robot_name = "sphero"
+        self.odom_pub = None
+        self.imu_pub = None
+        self.collision_pub = None
+        self.diag_pub = None
+        self.cmd_vel_sub = None
+        self.cmd_turn_sub = None
+        self.color_sub = None
+        self.back_led_sub = None
+        self.stabilization_sub = None
+        self.heading_sub = None
+        self.angular_velocity_sub = None
+        self.reconfigure_srv = None
+        self.transform_broadcaster = None
+        self._init_pubsub()
+
+        self.robot_name = None
         self.robot_bt_addr = None
-        if rospy.has_param('~bt_addr'):
-            self.robot_bt_addr = rospy.get_param('bt_addr')
-        else:
-            rospy.logerr(
-                "Must specify sphero address with private param bt_addr")
-            return
+        self.connect_color_red = None
+        self.connect_color_blue = None
+        self.connect_color_green = None
+        self.cmd_vel_timeout = None
+        self.diag_update_rate = None
+        self._init_params()
 
         rospy.loginfo("connect bt_addr %s", self.robot_bt_addr)
         self.robot = Sphero(self.robot_name, self.robot_bt_addr)
@@ -112,6 +126,7 @@ class SpheroNode(object):
             'collision', SpheroCollision, queue_size=1)
         self.diag_pub = rospy.Publisher(
             '/diagnostics', DiagnosticArray, queue_size=1)
+
         self.cmd_vel_sub = rospy.Subscriber(
             'cmd_vel', Twist, self.cmd_vel, queue_size=1)
         self.cmd_turn_sub = rospy.Subscriber(
@@ -131,6 +146,14 @@ class SpheroNode(object):
         self.transform_broadcaster = tf.TransformBroadcaster()
 
     def _init_params(self):
+        if rospy.has_param('~bt_addr'):
+            self.robot_bt_addr = rospy.get_param('bt_addr')
+        else:
+            rospy.logerr(
+                "Must specify sphero address with private param bt_addr")
+            sys.exit(1)
+
+        self.robot_name = rospy.get_param("~name", "Sphero")
         self.connect_color_red = rospy.get_param('~connect_red', 0)
         self.connect_color_blue = rospy.get_param('~connect_blue', 0)
         self.connect_color_green = rospy.get_param('~connect_green', 255)
@@ -143,25 +166,40 @@ class SpheroNode(object):
         return math.fmod(math.fmod(angle, 2.0 * math.pi) + 2.0 * math.pi, 2.0 * math.pi)
 
     def start(self):
-        try:
-            self.is_connected = self.robot.connect()
-            rospy.loginfo("Connect to Sphero with address: %s" %
-                          self.robot.bt.target_address)
-        except:
-            rospy.logerr("Failed to connect to Sphero.")
+
+        tries = 0
+        while not self.is_connected and tries < 5:
+            try:
+                self.is_connected = self.robot.connect()
+                rospy.loginfo("Connect to Sphero with address: %s",
+                              self.robot.bt.target_address)
+            except SpheroException as error:
+                rospy.logwarn(
+                    "Failed to connect to Sphero with error %s", error.messge)
+                tries += 1
+        if not self.is_connected:
+            rospy.logerr("Cannot connect to sphero")
             sys.exit(1)
+
         # setup streaming
-        self.robot.set_filtered_data_strm(self.sampling_divisor, 1, 0, True)
-        self.robot.add_async_callback(
-            sphero_driver.IDCODE['DATA_STRM'], self.parse_data_strm)
+        manager = DataStreamManager()
+        manager.acc = True
+        manager.imu_angle = True
+        manager.gyro = True
+        manager.quaternion = True
+        manager.odom = True
+        manager.velocity = True
+        self.robot.set_data_stream(manager, self.update_rate, response=True)
+        self.robot.register_sensor_callback(self.handle_data_strm)
+
         # setup power notification
-        self.robot.set_power_notify(True, False)
-        self.robot.add_async_callback(
-            sphero_driver.IDCODE['PWR_NOTIFY'], self.parse_power_notify)
+        self.robot.set_power_notification(True)
+        self.robot.register_power_callback(self.handle_power_notify)
+
         # setup collision detection
-        self.robot.config_collision_detect(1, 45, 110, 45, 110, 100, False)
-        self.robot.add_async_callback(
-            sphero_driver.IDCODE['COLLISION'], self.parse_collision)
+        self.robot.start_collision_detection(45, 110, 45, 110, 1000)
+        self.robot.register_collision_callback(self.handle_collision)
+
         # set the ball to connection color
         self.robot.set_rgb_led(
             self.connect_color_red, self.connect_color_green, self.connect_color_blue, 0, False)
@@ -177,7 +215,7 @@ class SpheroNode(object):
                     self.cmd_heading = 0
                     self.cmd_speed = 0
                     self.robot.roll(int(self.cmd_speed), int(
-                        self.cmd_heading), 1, False)
+                        self.cmd_heading))
             if (now - self.last_diagnostics_time) > self.diag_update_rate:
                 self.last_diagnostics_time = now
                 self.publish_diagnostics(now)
@@ -185,10 +223,10 @@ class SpheroNode(object):
 
     def stop(self):
         # tell the ball to stop moving before quiting
-        self.robot.roll(int(0), int(0), 1, False)
-        self.robot.shutdown = True
+        self.robot.stop()
         rospy.sleep(1.0)
-        self.is_connected = self.robot.disconnect()
+        self.is_connected = False
+        self.robot.disconnect()
         self.robot.join()
 
     def publish_diagnostics(self, time):
@@ -205,46 +243,50 @@ class SpheroNode(object):
 
         self.diag_pub.publish(diag)
 
-    def parse_collision(self, data):
+    def handle_collision(self, data):
         if self.is_connected:
             now = rospy.Time.now()
             collision = SpheroCollision()
             collision.header.stamp = now
-            collision.x = data["X"]
-            collision.y = data["Y"]
-            collision.z = data["Z"]
-            collision.axis = int(data["Axis"])
-            collision.x_magnitude = data["xMagnitude"]
-            collision.y_magnitude = data["yMagnitude"]
-            collision.speed = data["Speed"]
-            collision.timestamp = data["Timestamp"]
+            collision.x = data.x
+            collision.y = data.y
+            collision.z = data.z
+            collision.axis = data.axis
+            collision.x_magnitude = data.x_magnitude
+            collision.y_magnitude = data.y_magnitude
+            collision.speed = data.speed
+            collision.timestamp = data.timestamp
 
             self.collision = collision
             self.collision_pub.publish(self.collision)
 
-    def parse_power_notify(self, data):
+    def handle_power_notify(self, data):
         if self.is_connected:
             self.power_state = data
             self.power_state_msg = self.battery_state[data]
 
-    def parse_data_strm(self, data):
+    def handle_data_strm(self, data):
+        # TODO FIX THIS
         if self.is_connected:
             now = rospy.Time.now()
             imu = Imu(header=rospy.Header(frame_id="imu_link"))
             imu.header.stamp = now
-            imu.orientation.x = data["QUATERNION_Q0"]
-            imu.orientation.y = data["QUATERNION_Q1"]
-            imu.orientation.z = data["QUATERNION_Q2"]
-            imu.orientation.w = data["QUATERNION_Q3"]
-            imu.linear_acceleration.x = data["ACCEL_X_FILTERED"] / 4096.0 * 9.8
-            imu.linear_acceleration.y = data["ACCEL_Y_FILTERED"] / 4096.0 * 9.8
-            imu.linear_acceleration.z = data["ACCEL_Z_FILTERED"] / 4096.0 * 9.8
-            imu.angular_velocity.x = data["GYRO_X_FILTERED"] * \
-                10 * math.pi / 180
-            imu.angular_velocity.y = data["GYRO_Y_FILTERED"] * \
-                10 * math.pi / 180
-            imu.angular_velocity.z = data["GYRO_Z_FILTERED"] * \
-                10 * math.pi / 180
+
+            quaternion = data['quaternion']
+            imu.orientation.x = quaternion.x
+            imu.orientation.y = quaternion.y
+            imu.orientation.z = quaternion.z
+            imu.orientation.w = quaternion.w
+
+            acceleration = data['acc']
+            imu.linear_acceleration.x = acceleration.x
+            imu.linear_acceleration.y = acceleration.y
+            imu.linear_acceleration.z = acceleration.z
+
+            gyro = data['gyro']
+            imu.angular_velocity.x = gyro.x
+            imu.angular_velocity.y = gyro.y
+            imu.angular_velocity.z = gyro.z
 
             self.imu = imu
             self.imu_pub.publish(self.imu)
@@ -252,10 +294,15 @@ class SpheroNode(object):
             odom = Odometry(header=rospy.Header(frame_id="odom"),
                             child_frame_id='base_footprint')
             odom.header.stamp = now
-            odom.pose.pose = Pose(Point(
-                data["ODOM_X"] / 100.0, data["ODOM_Y"] / 100.0, 0.0), Quaternion(0.0, 0.0, 0.0, 1.0))
-            odom.twist.twist = Twist(Vector3(data["VELOCITY_X"] / 1000.0, 0, 0), Vector3(
-                0, 0, data["GYRO_Z_FILTERED"] * 10.0 * math.pi / 180.0))
+
+            odom = data['odom']
+            odom.pose.pose = Pose(Point(odom.x, odom.y, 0.0),
+                                  Quaternion(0.0, 0.0, 0.0, 1.0))
+
+            velocity = data['velocity']
+            odom.twist.twist = Twist(
+                Vector3(velocity.x, velocity.y, 0), Vector3(0, 0, gyro.z))
+
             odom.pose.covariance = self.ODOM_POSE_COVARIANCE
             odom.twist.covariance = self.ODOM_TWIST_COVARIANCE
             self.odom_pub.publish(odom)
@@ -263,11 +310,12 @@ class SpheroNode(object):
             # need to publish this trasform to show the roll, pitch, and yaw
             # properly
             self.transform_broadcaster.sendTransform((0.0, 0.0, 0.038),
-                                                     (data["QUATERNION_Q0"], data["QUATERNION_Q1"],
-                                                      data["QUATERNION_Q2"], data["QUATERNION_Q3"]),
+                                                     (quaternion.x, quaternion.y,
+                                                      quaternion.z, quaternion.w),
                                                      odom.header.stamp, "base_link", "base_footprint")
 
     def cmd_vel(self, msg):
+        # TODO start here
         if self.is_connected:
             self.last_cmd_vel_time = rospy.Time.now()
             self.cmd_heading = self.normalize_angle_positive(
